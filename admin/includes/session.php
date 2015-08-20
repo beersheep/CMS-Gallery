c0bccbcf683cf93188b83c945eef72d96b5a0874
beersheep <beersheep0109@gmail.com>
2015-08-20 11:43:44 +0800

    added check login method

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -6,12 +6,20 @@ Class Session{
 	public  $user_id;
 
 	function __construct(){
-
 	session_start();
 
+	}
 
+	private function check_login(){
 
+		if(isset($_session['user_id'])) {
+			$this->user_id = $_session['user_id'];
+			$this->signed_in = true;
+		} else {
 
+			unset($this->user_id);
+			$this->sign_in = false;
+		}
 	}
 
 }
